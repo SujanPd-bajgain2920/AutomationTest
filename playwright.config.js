65a42e480e036e62a7bf1ec716d39a9d08eb4631
SujanPd-bajgain2920 <sujanprasadbajgain731@gmail.com>
2026-03-17 13:13:16 +0545

    headless false

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     actionTimeout: 30000,       // 30s per action
     navigationTimeout: 30000,   // 30s for page.goto
